f1218a2d54b23cfe90cf0b47a3519823e53a11a4
maximilianb93 <maximilian-rudolf.becker@sorbonne-universite.fr>
2026-04-16 17:37:03 +0200

    fix: datamodule loads splits marginally > 1.0

--- mlcolvar/data/datamodule.py
+++ mlcolvar/data/datamodule.py
@@ -260,7 +260,7 @@ def split_dataset(
     Optionally fix the generator for reproducible results.
     """
 
-    if math.isclose(sum(lengths), 1) and sum(lengths) <= 1:
+    if math.isclose(sum(lengths), 1):
         subset_lengths = []
         for i, frac in enumerate(lengths):
             if frac < 0 or frac > 1:
@@ -271,9 +271,12 @@ def split_dataset(
             subset_lengths.append(n_items_in_split)
         remainder = len(dataset) - sum(subset_lengths)  # type: ignore[arg-type]
         # add 1 to all the lengths in round-robin fashion until the remainder is 0
-        for i in range(remainder):
+        for i in range(abs(remainder)):
             idx_to_add_at = i % len(subset_lengths)
-            subset_lengths[idx_to_add_at] += 1
+            if remainder > 0:
+                subset_lengths[idx_to_add_at] += 1
+            else:
+                subset_lengths[idx_to_add_at] -= 1
         lengths = subset_lengths
         for i, length in enumerate(lengths):
             if length == 0:
